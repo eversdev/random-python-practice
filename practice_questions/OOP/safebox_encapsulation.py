d75a3af23fb29043821a2710bc3a08fc2a94ac3d
B <beversprof25@outlook.com>
2025-06-19 11:01:56 +0100

    Make set_code method interactive with input validation loop.

--- practice_questions/OOP/safebox_encapsulation.py
+++ practice_questions/OOP/safebox_encapsulation.py
@@ -8,24 +8,45 @@ class SafeBox:
 
     The check_code method checks if the input matches the stored code and returns a message.
     """
+
     def __init__(self):
+        """Initialize SafeBox with a private __code attribute set to None."""
         self.__code = None
+
+    def set_code(self) -> str:
+        """
+        Set the safe box code.
+
+        Prompt the user to enter a 4-digit code.
         
-    def set_code(self, user_input):
-        self.__code = user_input
-        if len(user_input) > 4:
-            return "code you entered is exceeds 4 digits"
-        elif len(user_input) < 4:
-            return "code you entered is less than 4 digits"
-        else:
-            return user_input
-    
-    def check_code(self, user_input):
+        Returns:
+        str: A message indicating if the code is too long, too short, or successfully set.
+        """
+        
+        while True:
+
+            user_input = input("Enter a 4-digit code: ")
+
+            if len(user_input) > 4:
+                print("The code you entered exceeds 4 digits.")
+            elif len(user_input) < 4:
+                print("The code you entered is less than 4 digits.")
+            else:
+                self.__code = user_input
+                return f"You have entered {user_input} as your code."
+            
+
+    def check_code(self, user_input) -> str:
+        """
+        Check if the provided code matches the stored code.
+
+        Parameters:
+        user_input (str): The code to check.
+
+        Returns:
+        str: A message indicating if the entered code is correct or incorrect.
+        """
         if user_input == self.__code:
-            return "entered real one"
+            return "Code entered correctly."
         else:
-            return "entered incorrect code"
-
-
-
-
+            return "Incorrect code entered."
