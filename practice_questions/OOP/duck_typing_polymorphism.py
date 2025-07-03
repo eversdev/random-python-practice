2687c3a2e3febda1278ade8196cfe502f71d8b08
B <beversprof25@outlook.com>
2025-07-03 23:54:44 +0100

    Added instance tests to demonstrate duck typing polymorhpism with play_sound function

---
+++ practice_questions/OOP/duck_typing_polymorphism.py
@@ -0,0 +1,27 @@
+class Bird:
+    def make_sound(self):
+        return "Chirp"
+
+class Dog:
+    def make_sound(self):
+        print("Woof!")
+
+
+
+
+def play_sound(my_obj):
+    return my_obj.make_sound()
+
+
+
+
+b1 = Bird()
+print(play_sound(b1))
+
+print("-------")
+
+d1 = Dog()
+play_sound(d1)
+
+
+
