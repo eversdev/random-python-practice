ab5dfd5e32f60ac6ee81a8939bfb78c21c0158d9
B <beversprof25@outlook.com>
2025-07-04 23:54:19 +0100

    Add method availability demo with try/except to Vehicle polymorphism example

---
+++ practice_questions/OOP/vehicle_method_overriding_vs_new_method.py
@@ -0,0 +1,47 @@
+class Vehicle:
+    """
+    Base class representing a generic vehicle.
+    """
+    def move(self):
+        print("Vehicle Moves")
+
+
+
+class Car(Vehicle):
+    """
+    Subclass representing a car.
+    """
+    def move(self):
+        print("Car drives")
+
+    def stop(self):
+        print("Vehicle stops!")
+
+
+class Boat(Vehicle):
+    """
+    Subclass representing a boat.
+    """
+    def move(self):
+        print("Boat sails")
+
+   
+
+    
+
+
+def start_engine(my_obj):
+    """
+    Calls the move method of the passed-in object.
+
+    This shows polymorphism: different objects respond to the same method call.
+    """
+    my_obj.move()
+
+c1 = Car()
+b1 = Boat()
+
+try:
+    b1.stop()
+except AttributeError:
+    print("stop() method is not a method found in instance of class Boat")
