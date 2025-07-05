ec0457df016768f09a93bd1cb0eb1417e5a0d7b7
B <beversprof25@outlook.com>
2025-07-05 03:08:00 +0100

    Handle instantiation error for abstract Device class

--- practice_questions/OOP/abstract_base_device.py
+++ practice_questions/OOP/abstract_base_device.py
@@ -1,17 +1,32 @@
 from abc import ABC, abstractmethod
 
 class Device(ABC):
+    """"
+    An abstract base class that defines a generic device interface.
+    Requires subclasses to implement the start() method.
+    """
     @abstractmethod
     def start(self):
+        """
+        Abstract method meant to be overridden by subclasses.
+        Should define how the device starts.
+        """
         pass
 
 
 class Phone(Device):
-    def start(self):
+    """
+    Starts the phone by simulating dialing a number.
+    """
+    def start(self) -> str:
         print("Dial a number")
 
 
 
 p1 = Phone()
-
 p1.start()
+
+try:
+    d1 = Device()
+except TypeError as e:
+    print("Unable to instantiate abstract class", e)
