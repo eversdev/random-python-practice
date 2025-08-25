d6ce3ba666c4cd4a88e08b54aa89c8834bbe4764
B <beversprof25@outlook.com>
2025-08-26 00:08:00 +0100

    Wrap test code in __main__ block

--- practice_questions/random/app/aggregate_pairs.py
+++ practice_questions/random/app/aggregate_pairs.py
@@ -15,8 +15,7 @@ def aggregate_pairs(arr):
     return new_list
 
 
-my_list = ["A:2", "B:1", "X:3", "Y:0", "A:1", "X:0", "B:4"]
-
-
-my_func = aggregate_pairs(my_list)
-print(my_func)
+if __name__ == "__main__":
+    my_list = ["A:2", "B:1", "X:3", "Y:0", "A:1", "X:0", "B:4"]
+    my_func = aggregate_pairs(my_list)
+    print(my_func)
